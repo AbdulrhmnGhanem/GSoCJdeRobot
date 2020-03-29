5f845a939446fc3c9e536563d8b6c5ae773dd29c
Abdulrhman Ghanem <abdoghanem160@gmail.com>
2020-03-29 20:16:29 +0200

    Add: basic cell drawing

--- graphics.py
+++ graphics.py
@@ -1,15 +1,18 @@
 import turtle
+
+
 screen = turtle.getscreen()
 t = turtle.Turtle()
 t.ht()
-
 t.speed(1000)
-t.begin_fill()
-t.fd(10)
-t.rt(90)
-t.fd(10)
-t.rt(90)
-t.fd(10)
-t.rt(90)
-t.fd(10)
-t.end_fill()
+
+
+def dead_cell(edge_size=10):
+    for i in range(7):
+        t.fd(edge_size) if i % 2 == 0 else t.rt(90)
+
+
+def live_cell(edge_size=10):
+    t.begin_fill()
+    dead_cell(edge_size)
+    t.end_fill()
