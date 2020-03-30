9fbe80e0fd877216ca35538957b5b767f62d900c
Abdulrhman Ghanem <abdoghanem160@gmail.com>
2020-03-30 05:12:40 +0200

    Refactor: graphics.py

--- graphics.py
+++ graphics.py
@@ -1,18 +1,28 @@
 import turtle
 
 
-screen = turtle.getscreen()
-t = turtle.Turtle()
-t.ht()
-t.speed(1000)
+def change_pos(screen: turtle.TurtleScreen):
+    x, y = screen.window_width(), screen.window_height()
+    t.penup()
+    t.setpos(-x/2, y/2)
+    t.pendown()
 
 
-def dead_cell(edge_size=10):
+def dead_cell(edge_size: int = 10):
     for i in range(7):
         t.fd(edge_size) if i % 2 == 0 else t.rt(90)
 
 
-def live_cell(edge_size=10):
+def live_cell(edge_size: int = 10):
     t.begin_fill()
     dead_cell(edge_size)
     t.end_fill()
+
+
+if __name__ == '__main__':
+    screen = turtle.getscreen()
+    t = turtle.Turtle()
+    t.ht()
+    t.speed(10)
+    change_pos(screen)
+    live_cell()
